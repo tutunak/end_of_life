ce88ea353c5c43ea3e8b2ee8da276907835b2a8f
Dmitrii Kotov <dk@tutunak.com>
2023-04-30 15:33:43 +0400

    updated for cli work

--- eol.py
+++ eol.py
@@ -1,4 +1,7 @@
+import datetime
 import time
+import argparse
+
 
 import requests
 
@@ -18,11 +21,19 @@ def get_product(product_name) -> list[dict]:
     return __elo_api_call__(product_name=product_name)
 
 
+def arg_parser() -> argparse.Namespace:
+    parser = argparse.ArgumentParser(description='EOL')
+    parser.add_argument("--date", help="Date", default=datetime.date.today().strftime("%Y-%m-%d"))
+    args = parser.parse_args()
+    return args
+
+
 def main():
+    args = arg_parser()
     for product in get_all_products():
         for cycle in get_product(product):
             eol = cycle.get("eol", "")
-            if str(eol).startswith("2023-04-30"):
+            if str(eol).startswith(args.date):
                 print(f"{product} : Release: {cycle['cycle']} : {eol}")
         time.sleep(1)
 
